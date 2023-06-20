451ac69b1ebcd0299e855b2a9bfb8ce85a761684
MinhGCC200321 <minhtqgcc200321@fpt.edu.vn>
2023-06-20 22:03:25 +0700

    Add action OrderDetail and OrderExists in OrderController

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -69,5 +69,14 @@ namespace FPTBook.Controllers
             }
             return View(order);
         }
+
+        private bool OrderExists(int id)
+        {
+          return (_context.Order?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+        public async Task<IActionResult> OrderDetail(int id){
+            var fPTBookContext = _context.OrderItem.Where(e => e.Order.Id == id).Include(b => b.Book).Include(o => o.Order).Include(c => c.Book.Author);
+            return View(await fPTBookContext.ToListAsync());
+        }
     }
 }
