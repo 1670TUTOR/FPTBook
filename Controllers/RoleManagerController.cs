69aa26261f48b07158c6060388ecaa57a511e1b7
thulagcc <106128337+thulagcc@users.noreply.github.com>
2023-06-23 14:38:09 +0700

    Add index in RoleManagerController

--- Controllers/RoleManagerController.cs
+++ Controllers/RoleManagerController.cs
@@ -13,6 +13,15 @@ namespace FPTBook.Controllers
      [Authorize(Roles = "Admin")]
     public class RoleManagerController : Controller
     {
-       
+       private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleManagerController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
     }
 }
