d1df3b2e1ce073d2e8663005c50895f513c396e6
thulagcc <106128337+thulagcc@users.noreply.github.com>
2023-06-24 20:26:36 +0700

    Add Index in UserRolesController

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -15,6 +15,30 @@ namespace FPTBook.Controllers
     [Authorize(Roles = "Admin")]
     public class UserRolesController : Controller
     {
-        
+        private readonly UserManager<BookUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesController(UserManager<BookUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+            foreach (BookUser user in users)
+            {
+                var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.Email = user.Email;
+                thisViewModel.Name = user.Name;
+                thisViewModel.DOB = user.DOB;
+                thisViewModel.Address = user.Address;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+            }
+            return View(userRolesViewModel);
+        }
     }
 }
